65c4cb06b361a9729397ef709b774f44353954ac
semihseven <semihseven0@gmail.com>
2025-09-27 13:01:25 +0300

    isPerfectNumber created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println(isPalindrome(-1221));
-        System.out.println(isPalindrome(707));
-        System.out.println(isPalindrome(11212));
+        //System.out.println(isPalindrome(-1221));
+        //System.out.println(isPalindrome(707));
+        //System.out.println(isPalindrome(11212));
+
+
+        //System.out.println(isPerfectNumber(6));
+        //System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        //System.out.println(isPerfectNumber(-1));
+
     }
 
     public static boolean isPalindrome (int num) {
@@ -18,6 +25,24 @@ public class Main {
             //System.out.println(reversed);
         }
         return number.equals(reversed);
+    }
+
+    public static boolean isPerfectNumber(int num) {
+        int result=0;
+        if (num<0) {
+            return false;
+        }
+
+        for (int i=1; i<=num/2; i++) {
+            if(num%i == 0) {
+                result+=i;
+                System.out.println("bölen: "+i);
+
+            }
+            System.out.println("toplam: "+result);
+        }
+        return result == num;
+
     }
 
 }
