6a08aa8825be6e45ba26fd2d6318df9406186653
semihseven <semihseven0@gmail.com>
2025-09-27 14:03:15 +0300

    tests passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,9 +14,9 @@ public class Main {
         //System.out.println(isPerfectNumber(5));
         //System.out.println(isPerfectNumber(-1));
 
-        System.out.println(numberToWords(123));
-        System.out.println(numberToWords(1010));
-        System.out.println(numberToWords(-12));
+        //System.out.println(numberToWords(123));
+        //System.out.println(numberToWords(1010));
+        //System.out.println(numberToWords(-12));
 
     }
 
